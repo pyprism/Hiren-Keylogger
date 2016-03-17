7cb0334023264ea90f607e3f0a6ec455581cf682
Ashutosh Das <git.pyprism@gmail.com>
2016-03-17 12:01:18 +0600

    initial commit

---
+++ keyConstants.h
@@ -0,0 +1,21 @@
+#ifndef KEYCONSTANTS_H
+#define KEYCONSTANTS_H
+
+#include <map>
+#include <string>
+
+class KeyPair {
+public:
+    KeyPair(const std::string &vk = "", const std::string &name= "") : VKName (vk), Name (name) {}
+
+    std::string VKName;
+    std::string Name;
+};
+
+class Keys {
+public:
+    static std::map<int, KeyPair> Keys;
+};
+
+// http://www.kbdedit.com/manual/low_level_vk_list.html
+#endif // KEYCONSTANTS_H
